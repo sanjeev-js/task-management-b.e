f69ab1da865d0f5a2539444be9f3ebc0921c7bca
Achintya Singh <anmolrai109@gmail.com>
2025-01-27 23:50:49 +0530

    Added CORS

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"task-management/config"
+	"task-management/middlewares"
 	"task-management/migrations"
 	"task-management/routes"
 
@@ -13,7 +14,17 @@ func main() {
 	migrations.Migrate()
 
 	r := gin.Default()
+	r.Use(middlewares.CorsConfig())
+
 	routes.RegisterRoutes(r)
 
+	r.POST("/login", loginHandler)
+
 	r.Run(":8080")
+}
+
+//Checking if the route is working
+func loginHandler(c *gin.Context){
+	c.JSON(200,gin.H{"message":"Successfully logged in"})
+
 }
