f1884e3e849807027dbe3de0b550ad72bb691016
Achintya Singh <anmolrai109@gmail.com>
2025-01-28 17:40:27 +0530

    verified the routes based on roles with static details

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"net/http"
 	"task-management/config"
+	"task-management/middlewares"
 	"task-management/migrations"
 	"task-management/routes"
 
@@ -13,7 +15,47 @@ func main() {
 	migrations.Migrate()
 
 	r := gin.Default()
+	r.Use(middlewares.CorsConfig())
+
 	routes.RegisterRoutes(r)
 
+	r.POST("/login", staticCheckUser)
+
 	r.Run(":8080")
+}
+
+//Checking if the route is working
+var staticUser = map[string]struct {
+	Password string
+	Role     string
+}{
+	"admin@gmail.com": {Password: "admin@123", Role: "admin"},
+	"user@gmail.com":  {Password: "user@123", Role: "user"},
+}
+
+func staticCheckUser(c *gin.Context) {
+	var input struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	//Bind Json Input
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	//Validate Credentials
+
+	user, exists := staticUser[input.Email]
+	if !exists || user.Password != input.Password {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid Credentials"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"role": user.Role,
+	})
+
 }
